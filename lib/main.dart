12c7e45537fe2a02dc09cffdbf5a9d626cc827e7
Amr Abdelmagid <amrabdelmagid900@gmail.com>
2021-04-28 00:07:47 +0200

    provider pattern was used

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:untitled1/providers/products_provider.dart';
 import 'package:untitled1/screens/product_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,16 +11,19 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shop App',
-      theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: 'Lato'),
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName : (context) => ProductDetailScreen()
-      },
+    return ChangeNotifierProvider(
+      create: (context) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'Shop App',
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato'),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName : (context) => ProductDetailScreen()
+        },
+      ),
     );
   }
 }
