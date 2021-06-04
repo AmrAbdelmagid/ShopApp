508c1a2e08b0be0cc9ee4d110b2bd9e69a8fd310
Amr Abdelmagid <amrabdelmagid900@gmail.com>
2021-06-04 15:37:41 +0200

    Added authentication screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:untitled1/providers/cart.dart';
 import 'package:untitled1/providers/orders_provider.dart';
 import 'package:untitled1/providers/products_provider.dart';
+import 'package:untitled1/screens/auth_screen.dart';
 import 'package:untitled1/screens/cart_screen.dart';
 import 'package:untitled1/screens/edit_product_screen.dart';
 import 'package:untitled1/screens/orders_screen.dart';
@@ -36,13 +37,14 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.purple,
             accentColor: Colors.deepOrange,
             fontFamily: 'Lato'),
-        home: ProductOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
           CartScreen.routeName: (context) => CartScreen(),
           OrdersScreen.routeName: (context) => OrdersScreen(),
           UserProductScreen.routeName: (context) => UserProductScreen(),
           EditProductScreen.routeName: (context) => EditProductScreen(),
+          AuthScreen.routeName: (context) => AuthScreen(),
         },
       ),
     );
